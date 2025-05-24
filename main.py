d7134bb7d50368b3036a3d6d789ad3f1503175ed
KritishBokde91 <kritishbokde.official@gmail.com>
2025-05-24 14:37:09 +0530

    V1.1.3

--- main.py
+++ main.py
@@ -241,5 +241,4 @@ async def clear_reference():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port, timeout_keep_alive=30)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
